5073220a79c043ee9cccdd3116a37f05c8b0c0a0
bidianqing <bidianqing@longfor.com>
2021-12-21 09:40:40 +0800

    *

--- src/Portal/Infrastructure/ServiceCollectionExtensions.cs
+++ src/Portal/Infrastructure/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace Portal.Infrastructure
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
