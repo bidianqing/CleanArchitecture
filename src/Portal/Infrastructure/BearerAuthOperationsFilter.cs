b84f8ebe8cbb3d52ad0719b07f0daac13dd54976
bidianqing <bidianqing@longfor.com>
2022-01-05 18:03:24 +0800

    swagger

---
+++ src/Portal/Infrastructure/BearerAuthOperationsFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Portal.Infrastructure
+{
+    public class BearerAuthOperationsFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var noAuthRequired = context.ApiDescription.CustomAttributes().Any(attr => attr.GetType() == typeof(AllowAnonymousAttribute));
+
+            if (noAuthRequired) return;
+
+            operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                }
+            };
+        }
+    }
+}
--- src/Portal/Startup.cs
+++ src/Portal/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Portal.Infrastructure;
+using System;
+using System.IO;
 using System.Reflection;
 
 namespace Portal
@@ -29,8 +34,27 @@ namespace Portal
                 options.DatabaseType = DatabaseType.SqlServer;
             });
 
-            //services.AddScoped<IOrderService, OrderService>();
-            //services.AddScoped<IOrderRepository, OrderRepository>();
+            // Register the Swagger generator, defining one or more Swagger documents
+            // https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/getting-started-with-swashbuckle
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Portal.xml"), true);
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Domain.xml"), true);
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Infrastructure.xml"), true);
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = HeaderNames.Authorization,
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    Description = "在下框中输入Bearer 你的jwt token",
+                });
+
+                c.OperationFilter<BearerAuthOperationsFilter>();
+            });
 
             services.AddAllRegisterTypes(Assembly.Load("Domain"), Assembly.Load("Infrastructure"));
 
@@ -47,6 +71,15 @@ namespace Portal
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
